fc024f0f065527fd2745c614fb070aea886e0072
spancavil <soancavil@gmail.com>
2021-12-28 14:54:29 -0300

    Fix styles on MyOrders

--- screens/MyOrders.js
+++ screens/MyOrders.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
 
 export const MyOrders = ({orders}) => {
     console.log(orders);
     return (
-        <View>
+        <View style={styles.container}>
             <FlatList
             data={orders}
             keyExtractor={order => order.id.toString()}
             renderItem={({item})=> {
-                return <View>
-                    <Text>ID: {item.id}</Text>
-                    <Text>Total: ${item.total}</Text>
-                    <Text>Fecha: {item.date}</Text>
+                return <View style={styles.ordersContainer}>
+                    <Text style={styles.text}>ID: {item.id}</Text>
+                    <Text style={styles.text}>Total: ${item.total.toFixed(2)}</Text>
+                    <Text style={styles.text}>Fecha: {item.date}</Text>
                 </View>
             }}
             ></FlatList>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width: '100vw'
+    },
+    ordersContainer: {
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        backgroundColor: 'gray',
+        marginVertical: 10,
+        // width: '100%',
+        padding: 7,
+        borderRadius: 6,
+    },
+    text: {
+        fontSize: 12,
+    }
+})
